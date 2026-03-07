e4f4236bb38dd171f1b035e56cffae45bbeff912
Daniel <danigora1515@gmail.com>
2026-03-07 19:38:23 +0100

    ajustes para render

--- app.py
+++ app.py
@@ -66,6 +66,8 @@ def performance():
         pair_winrates=pair_winrates
     )
 
+import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
